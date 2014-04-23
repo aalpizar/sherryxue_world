4a60bcf046f5346c381148a2e3f76165c48c6ffe
Audrey <aalpizar@andrew.cmu.edu>
2014-04-22 20:33:26 -0400

    Updated category model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,10 +2,10 @@ class Article < ActiveRecord::Base
 	belongs_to :category
 	
 	#Validations
-	validates_presence_of :tite, :content
+	validates_presence_of :title, :content
 
 	#Scope
 	scope :active, where('active = ?', true)
 	scope :alphabetical, order('title')
-	
+
 end
